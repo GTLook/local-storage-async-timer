1419c468f3174554110e203c5aa7570a7d040d6c
Gavin Look <GTLook@gmail.com>
2018-03-02 11:24:58 -0800

    added local storage and restart on double stop.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,39 @@
 console.log('JS Loaded!')
-let seconds = 0
+let seconds = localStorage.getItem('timer') || 0
+let running = false
+let inc = 0
 
 document.querySelector('#title span').innerHTML = seconds
+
+//document.querySelector(.btn)
+
+// setInterval(function (){
+//   seconds++
+//   document.querySelector('#title span').innerHTML = seconds
+// },1000)
+
+const buttons = document.querySelectorAll('.btn')
+const startButton = buttons[0]
+const stopButton = buttons[1]
+
+startButton.addEventListener('click', function () {
+  if(running == false){
+    running = true
+  inc = setInterval(function (){
+    seconds++
+    localStorage.setItem('timer',seconds)
+    document.querySelector('#title span').innerHTML = seconds
+  },1000)
+}
+})
+
+stopButton.addEventListener('click', function () {
+  if(running){
+    running = false
+    clearInterval(inc)
+  } else {
+    seconds = 0;
+    localStorage.setItem('timer',seconds)
+    document.querySelector('#title span').innerHTML = seconds
+  }
+})
